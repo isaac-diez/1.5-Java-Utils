8456ce0fb0423021a331e5366a093e4fea6b2104
isaac.diez <isaac.diez@proton.me>
2025-01-26 10:38:57 +0100

    feat: Level 1 Exer 5: Add Java object serialization and deserialization
    - Implemented functionality to serialize a Java object and save it to a `.ser` file.
    - Added functionality to deserialize the object from the `.ser` file back into memory.
    - Included error handling for file operations and serialization/deserialization processes.

---
+++ src/Level1/java/org/level1Exer5/Main/Main.java
@@ -0,0 +1,38 @@
+package org.level1Exer5.Main;
+
+import org.level1Exer5.Model.Person;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Isaac", 47);
+
+        try (FileOutputStream fileOut = new FileOutputStream("person.ser")) {
+
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(person);
+            System.out.println("Serialized object \""+person+"\" and saved to person.ser");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+            // e.printStackTrace();
+
+        }
+
+
+        try (FileInputStream fileIn = new FileInputStream("person.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            person = (Person) in.readObject();
+            System.out.println("Object deserialized: \"" + person + "\"");
+
+    } catch (Exception e) {
+            throw new RuntimeException(e);
+            // e.printStackTrace();
+        }
+
+    }
+
+}
